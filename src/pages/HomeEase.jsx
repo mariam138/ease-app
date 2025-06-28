efa606d6d1c4613852a30a1577506bf5bc2727c1
Mariam <mar.ayoub98@gmail.com>
2025-06-28 16:10:34 +0100

    dynamically update dropdown label with washing machine model

--- src/pages/HomeEase.jsx
+++ src/pages/HomeEase.jsx
@@ -1,8 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Scanner } from "@yudiel/react-qr-scanner";
 import { Dropdown, DropdownItem } from "flowbite-react";
 
 function HomeEase() {
+  const [selectedModel, setSelectedModel] = useState("Select your model");
+  const handleSelect = (model) => {
+    setSelectedModel(model);
+  };
+
   return (
     <>
       <p className="text-xl">Scan QR Code</p>
@@ -13,12 +18,22 @@ function HomeEase() {
       <Scanner onScan={(result) => console.log(result)} />
       <p className="text-sm text-gray-500">or search below for your model:</p>
       <div className="flex justify-center">
-        <Dropdown label="Select your model" dismissOnClick={false}>
-          <DropdownItem>LG WM4000HWA</DropdownItem>
-          <DropdownItem>Samsung WF45T6000AW</DropdownItem>
-          <DropdownItem>Whirlpool WTW5000DW</DropdownItem>
-          <DropdownItem>Maytag MVWC565FW</DropdownItem>
-          <DropdownItem>GE GTW840CSNWS</DropdownItem>
+        <Dropdown label={selectedModel} dismissOnClick={false}>
+          <DropdownItem onClick={() => handleSelect("LG WM4000HWA")}>
+            LG WM4000HWA
+          </DropdownItem>
+          <DropdownItem onClick={() => handleSelect("Samsung WF45T6000AW")}>
+            Samsung WF45T6000AW
+          </DropdownItem>
+          <DropdownItem onClick={() => handleSelect("Whirlpool WTW5000DW")}>
+            Whirlpool WTW5000DW
+          </DropdownItem>
+          <DropdownItem onClick={() => handleSelect("Maytag MVWC565FW")}>
+            Maytag MVWC565FW
+          </DropdownItem>
+          <DropdownItem onClick={() => handleSelect("GE GTW840CSNWS")}>
+            GE GTW840CSNWS
+          </DropdownItem>
         </Dropdown>
       </div>
     </>
