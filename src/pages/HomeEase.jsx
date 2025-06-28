179ac41fbfbc8df345419d3f01a361c27f9b7776
Mariam <mar.ayoub98@gmail.com>
2025-06-28 15:53:40 +0100

    add dropdown button

--- src/pages/HomeEase.jsx
+++ src/pages/HomeEase.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { Scanner } from "@yudiel/react-qr-scanner";
+import { Dropdown, DropdownItem } from "flowbite-react";
 
 function HomeEase() {
   return (
@@ -10,6 +11,15 @@ function HomeEase() {
 
       {/* QR Code Scanner Container */}
       <Scanner onScan={(result) => console.log(result)} />
+      <p className="text-sm text-gray-500">or search below for your model:</p>
+      <div className="flex justify-center">
+        <Dropdown label="Dropdown button" dismissOnClick={false}>
+          <DropdownItem>Dashboard</DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+          <DropdownItem>Earnings</DropdownItem>
+          <DropdownItem>Sign out</DropdownItem>
+        </Dropdown>
+      </div>
     </>
   );
 }
