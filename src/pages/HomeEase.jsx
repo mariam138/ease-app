0be7793f64390107267e03673fcb25a0731d26ad
Mariam <mar.ayoub98@gmail.com>
2025-06-28 16:40:31 +0100

    maint: remove useEffect hook

--- src/pages/HomeEase.jsx
+++ src/pages/HomeEase.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Scanner } from "@yudiel/react-qr-scanner";
 import { Dropdown, DropdownItem, Button } from "flowbite-react";
 
