c2243ee35758a464c9540d10140a50a25d76979b
Mariam <mar.ayoub98@gmail.com>
2025-06-28 15:10:33 +0100

    add text to homepage

--- src/pages/HomeEase.jsx
+++ src/pages/HomeEase.jsx
@@ -4,6 +4,8 @@ function HomeEase() {
   return (
     <>
       <p className="text-xl">Scan QR Code</p>
+      <br />
+      <p className="text-sm text-gray-500">Please scan the machine QR code</p>
     </>
   );
 }
