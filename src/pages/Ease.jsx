720500f2e0df674b90f421cb9d99cc2e8dd59258
Mariam <mar.ayoub98@gmail.com>
2025-06-29 11:05:24 +0100

    create add appliance btn

--- src/pages/Ease.jsx
+++ src/pages/Ease.jsx
@@ -303,6 +303,22 @@ function HomeEase() {
             />
             <span className="text-sm font-medium">Steamer</span>
           </button>
+          {/* Add appliance */}
+          <button className="flex flex-col items-center p-4 bg-gray-100 rounded-lg shadow hover:bg-blue-100 transition">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="w-10 h-10 mb-2"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14Zm.75-10.25v2.5h2.5a.75.75 0 0 1 0 1.5h-2.5v2.5a.75.75 0 0 1-1.5 0v-2.5h-2.5a.75.75 0 0 1 0-1.5h2.5v-2.5a.75.75 0 0 1 1.5 0Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <span className="text-sm font-medium">Add new</span>
+          </button>
         </div>
       </div>
 
